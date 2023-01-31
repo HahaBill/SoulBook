967cdef2a1822a05e9fd3969bc37378fe210260c
HaHaBill <bill.nguyentonhoang@live.com>
2023-01-31 23:00:09 +0100

    Connected a server to client

--- client/src/ApolloProvider.js
+++ client/src/ApolloProvider.js
@@ -6,5 +6,16 @@ import { createHttpLink } from 'apollo-link-http'
 import { ApolloProvider } from '@apollo/react-hooks'
 
 const httpLink = createHttpLink({
-    
+    uri: 'http://localhost:5001'
 })
+
+const client = new ApolloClient({
+    link: httpLink,
+    cache: new InMemoryCache()
+}) 
+
+export default (
+    <ApolloProvider client ={client}>
+        <App/>
+    </ApolloProvider>
+)
