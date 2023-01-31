78a9bb3b19aab4727f5135ce570750243481e86b
HaHaBill <bill.nguyentonhoang@live.com>
2023-02-01 00:47:39 +0100

    Set up basic routers

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
+import 'semantic-ui-css/semantic.min.css'
 
 function App() {
   return (
-    <div>
-      <h1>SoulBook</h1>
-    </div>
+    <Router>
+      <Router exact path='/' component={Home}/>
+      <Router exact path='/login' component={Login}/>
+      <Router exact path='/register' component={Register}/>
+    </Router>
   );
 }
 
