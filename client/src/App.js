51ffbd2bbc776fdd066cb56a8de8dc4beeccd620
HaHaBill <bill.nguyentonhoang@live.com>
2023-02-26 01:34:19 +0100

    SemanticUI + React Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
-import 'semantic-ui-css/semantic.min.css'
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import 'semantic-ui-css/semantic.min.css';
+import { Container } from 'semantic-ui-react';
+
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import MenuBar from './components/MenuBar';
 
 function App() {
   return (
-    <Router>
-      <Router exact path='/' component={Home}/>
-      <Router exact path='/login' component={Login}/>
-      <Router exact path='/register' component={Register}/>
-    </Router>
+    <div>
+      <Router>
+        <Container>
+          <MenuBar/>    
+          <Routes>
+            <Route exact path='/' element={<Home/>}/>
+            <Route exact path='/login' element={<Login/>}/>
+            <Route exact path='/register' element={<Register/>}/>
+          </Routes>
+        </Container>
+      </Router>
+
+    </div>
+
   );
 }
 
